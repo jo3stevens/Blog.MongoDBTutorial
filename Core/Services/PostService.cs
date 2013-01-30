caea82bcd824c6690e3d7fb57d53665aeb4da020
jo3stevens <joe.stevens@outlook.com>
2013-01-30 17:25:50 +1100

    Adding solution

---
+++ Core/Services/PostService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using Core.Domain;
+using MongoDB.Driver.Builders;
+using MongoDB.Bson;
+using System.Text.RegularExpressions;
+using System.Web;
+using Core.Helpers;
+using System.Diagnostics;
+
+namespace Core.Services
+{
+    public class PostService
+    {
+        private readonly MongoHelper<Post> _posts;
+
+        public PostService()
+        {
+            _posts = new MongoHelper<Post>();
+        }
+
+        public void Create(Post post)
+        {
+            post.Comments = new List<Comment>();
+            _posts.Collection.Save(post);
+        }
+
+        public bool Edit(Post post)
+        {
+            var result = _posts.Collection.FindAndModify(
+                Query.And(Query.EQ("_id", post.PostId), Query.EQ("Version", post.Version)),
+                null,
+                Update.Set("Title", post.Title)
+                    .Set("Url", post.Url)
+                    .Set("Summary", post.Summary)
+                    .Set("Details", post.Details)
+                    .Inc("Version", 1));
+
+            return result.ModifiedDocument != null;
+        }
+
+        public void Delete(ObjectId postId)
+        {
+            _posts.Collection.Remove(Query.EQ("_id", postId));
+        }
+
+        public IList<Post> GetPosts()
+        {
+            return _posts.Collection.FindAll().SetFields(Fields.Exclude("Comments")).SetSortOrder(SortBy.Descending("Date")).ToList();
+        }
+
+        public Post GetPost(ObjectId id)
+        {
+            var post = _posts.Collection.Find(Query.EQ("_id", id)).SetFields(Fields.Slice("Comments", -5)).Single();
+            post.Comments = post.Comments.OrderByDescending(c => c.Date).ToList();
+            return post;
+        }
+
+        public Post GetPost(string url)
+        {
+            var post = _posts.Collection.Find(Query.EQ("Url", url)).SetFields(Fields.Slice("Comments", -5)).Single();
+            post.Comments = post.Comments.OrderByDescending(c => c.Date).ToList();
+            return post;
+        }
+    }
+}
---
+++ MongoDBTutorial/Controllers/PostController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Domain;
+using Core.Services;
+using MongoDBTutorial.Helpers;
+using MongoDB.Bson;
+
+namespace MongoDBTutorial.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly PostService _postService;
+        private readonly CommentService _commentService;
+
+        public PostController()
+        {
+            _postService = new PostService();
+            _commentService = new CommentService();
+        }
+
+        public ActionResult Index()
+        {
+            return View(_postService.GetPosts());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.Url = post.Title.GenerateSlug();
+                post.Author = User.Identity.Name;
+                post.Date = DateTime.Now;
+
+                _postService.Create(post);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Update(string id)
+        {
+            return View(_postService.GetPost(id));
+        }
+
+        [HttpGet]
+        public ActionResult Delete(ObjectId id)
+        {
+            return View(_postService.GetPost(id));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult ConfirmDelete(ObjectId id)
+        {
+            _postService.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Update(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.Url = post.Title.GenerateSlug();
+
+                if (_postService.Edit(post))
+                {
+                    return RedirectToAction("Index");    
+                }
+
+                ModelState.AddModelError("ConcurrencyError", "This post has been updated since you started editing it. Please reload the post to get the latest changes.");
+                return View(post);
+            }
+
+            return View(post);
+        }
+
+        [HttpGet]
+        public ActionResult Detail(string id)
+        {
+            var post = _postService.GetPost(id);
+            ViewBag.PostId = post.PostId;
+
+            ViewBag.TotalComments = post.TotalComments;
+            ViewBag.LoadedComments = 5;
+
+            return View(post);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(ObjectId postId, Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                var newComment = new Comment()
+                                        {
+                                            CommentId = ObjectId.GenerateNewId(),
+                                            Author = User.Identity.Name,
+                                            Date = DateTime.Now,
+                                            Detail = comment.Detail
+                                        };
+
+                _commentService.AddComment(postId, newComment);
+
+                ViewBag.PostId = postId;
+                return Json(
+                    new
+                        {
+                            Result = "ok",
+                            CommentHtml = RenderPartialViewToString("Comment", newComment),
+                            FormHtml = RenderPartialViewToString("AddComment", new Comment())
+                        });
+            }
+
+            ViewBag.PostId = postId;
+            return Json(
+                new
+                    {
+                        Result = "fail",
+                        FormHtml = RenderPartialViewToString("AddComment", comment)
+                    });
+        }
+
+        public ActionResult RemoveComment(ObjectId postId, ObjectId commentId)
+        {
+            _commentService.RemoveComment(postId, commentId);
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult CommentList(ObjectId postId, int skip, int limit, int totalComments)
+        {
+            ViewBag.TotalComments = totalComments;
+            ViewBag.LoadedComments = skip + limit;
+            return PartialView(_commentService.GetComments(postId, ViewBag.LoadedComments, limit, totalComments));
+        }
+
+        /// <summary>
+        /// http://craftycodeblog.com/2010/05/15/asp-net-mvc-render-partial-view-to-string/
+        /// </summary>
+        /// <param name="viewName"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        protected string RenderPartialViewToString(string viewName, object model)
+        {
+            if (string.IsNullOrEmpty(viewName))
+                viewName = ControllerContext.RouteData.GetRequiredString("action");
+
+            ViewData.Model = model;
+
+            using (StringWriter sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                ViewContext viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
